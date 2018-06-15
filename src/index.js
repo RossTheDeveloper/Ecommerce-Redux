860fc6afb492e3bbf939457b2118d6305a95998e
Ross Shtivelberg <RossTheDev@gmail.com>
2018-06-14 20:45:39 -0400

    redux store added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+import initialState from './store/initialState'
+import appReducer from './store/reducers'
 
+const store = createStore(appReducer,initialState)
 
-
+console.log("hi", store.getState())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
