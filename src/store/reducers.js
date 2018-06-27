725638b5fb1d8fc7f843f346862e3b236cacdcbb
Ross Shtivelberg <RossTheDev@gmail.com>
2018-06-26 23:49:53 -0400

    redux cleanup

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,26 +4,11 @@ import { combineReducers } from 'redux'
 // cart -check if already contained, edit amount if is
 //make sure remove working.
 
-//     case C.ADD_DAY :
-//
-//       const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
-//
-//       return (hasDay) ?
-//          state :
-//          [
-//            ...state,
-//            skiDay(null, action)
-//          ].sort((a, b) => new Date(b.date) - new Date(a.date))
 
 export const cart = (state=[], action) => {
   switch(action.type) {
 
-
-
-
       case C.ADD_TO_CART :
-
-
       	return [
            ...state,
            action.payload
@@ -38,99 +23,10 @@ export const cart = (state=[], action) => {
 
 export const products = (state=[]) => state
 
-// export const goal = (state=10, action) =>
-// 	(action.type === C.SET_GOAL) ?
-// 		 parseInt(action.payload) :
-// 		 state
-//
-// export const skiDay = (state=null, action) =>
-//   (action.type === C.ADD_DAY) ?
-//   	action.payload :
-//   	state
-//
-// export const errors = (state=[], action) => {
-//   switch(action.type) {
-//     case C.ADD_ERROR :
-//     	return [
-//          ...state,
-//          action.payload
-//     	]
-//     case C.CLEAR_ERROR :
-//       return state.filter((message, i) => i !== action.payload)
-//   	default:
-//   		return state
-//   }
-// }
-//
-// export const allSkiDays = (state=[], action) => {
-//
-//   switch(action.type) {
-//
-//     case C.ADD_DAY :
-//
-//       const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
-//
-//       return (hasDay) ?
-//          state :
-//          [
-//            ...state,
-//            skiDay(null, action)
-//          ].sort((a, b) => new Date(b.date) - new Date(a.date))
-//
-//     case C.REMOVE_DAY :
-//
-//       return state.filter(skiDay => skiDay.date !== action.payload)
-//
-//     default:
-//       return state
-//   }
-//
-// }
-//
-// export const fetching = (state=false, action) => {
-//
-//   switch(action.type) {
-//
-//     case C.FETCH_RESORT_NAMES :
-//       return true
-//
-//     case C.CANCEL_FETCHING :
-//       return false
-//
-//     case C.CHANGE_SUGGESTIONS :
-//       return false
-//
-//     default:
-//       return state
-//   }
-//
-// }
-//
-// export const suggestions = (state=[], action) => {
-//
-//   switch(action.type) {
-//
-//     case C.CLEAR_SUGGESTIONS :
-//       return []
-//
-//     case C.CHANGE_SUGGESTIONS :
-//       return action.payload
-//
-//     default :
-//       return state
-//   }
-//
-// }
 
-// export default combineReducers({
-//   allSkiDays,
-//   goal,
-//   errors,
-//   resortNames: combineReducers({
-//     fetching,
-//     suggestions
-//   })
-// })
+
+
+
 
 export default combineReducers({
   cart,
